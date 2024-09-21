dee6009fc1336a93822899218228e1fd0b3e150f
Alejandro Botero <alejandro@albotero.com>
2024-09-20 23:58:28 -0500

    Html code refactor

--- assets/js/loadProperties.js
+++
@@ -1,9 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search)
-const pageType = urlParams.get("type")
-const data = [
-  {
-    propType: pageType,
-    sortBy: pageType === "rent" ? "costo" : "dateAdded",
-  },
-]
-document.querySelector(`.link-${pageType}`).classList.add("active")
